8908d805d4d3fed763c26a2f8fe1ed78c21d02ee
Davi Nunes <futurodavi@hotmail.com>
2013-01-27 06:26:29 -0200

    changes

---
+++ prototype/js/backend.js
@@ -0,0 +1,189 @@
+var backend = (function($){
+
+          var items = [];
+          var recommendations = [];
+          var notifications = [];
+
+          var limit = 15;
+
+          var urls = {
+            "recommendations": "http://localhost/bookschange/shielded-sierra-1174/backend/items/recommendations/{1}",
+            "items":"http://localhost/bookschange/shielded-sierra-1174/backend/items/{1}",
+            "itemsSearch": "http://localhost/bookschange/shielded-sierra-1174/items/search/{1}",
+            "itemsAdd":"http://localhost/bookschange/shielded-sierra-1174/backend/items/",
+            "itemsUpdate":"http://localhost/bookschange/shielded-sierra-1174/backend/items/{1}",
+            "itemsDelte":"http://localhost/bookschange/shielded-sierra-1174/backend/items/{1}",
+            "notifications":"http://localhost/bookschange/shielded-sierra-1174/backend/notifications/",
+            "notificationsAdd":"http://localhost/bookschange/shielded-sierra-1174/backend/notifications/",
+            "notificationsUpdate":"http://localhost/bookschange/shielded-sierra-1174/backend/notifications/{1}",
+            "notificationsDelete":"http://localhost/bookschange/shielded-sierra-1174/backend/notifications/{1}"
+          };
+
+          var methods = {
+
+            recommendations : {
+              get:function(){
+                return recommendations;
+              },
+
+              fetch:function(limit){
+                limit = limit || limit;
+                var url = tokenReplace(urls,recommendations,[limit]);
+
+                $.get(url, item, function(data){
+                    recommendations = JSON.parse(data);
+                },this).error(function(){console.log("Unable to reach backend")});
+              }
+
+            },
+
+            items : {
+
+              get: function(){
+                return items;
+              },
+
+              fetch: function(limit){
+                limit = limit || limit;
+                var url = tokenReplace(urls.recommendations,[limit]);
+
+                $.get(url, item, function(data){
+                    items = JSON.parse(data);
+                },this).error(function(){console.log("Unable to reach backend")});
+              },
+
+              add: function(item){
+
+                $.ajax({
+                    type: 'POST',
+                    contentType: 'application/json',
+                    url: urls.itemsAdd,
+                    dataType: "json",
+                    data: JSON.stringify(item),
+                    success: function(data){
+                      items.push(item);
+                    },
+                    error: function(jqXHR, textStatus, errorThrown){
+                        console.log("Unable to reach backend");
+                    }
+                });
+              },
+
+              update: function(id, newItem){
+                var url = tokenReplace(urls.itemsUpdate,[id]);
+                $.ajax({
+                    type: 'PUT',
+                    contentType: 'application/json',
+                    url: url,
+                    dataType: "json",
+                    data: newItem,
+                    success: function(data){
+                      _.detect(items,function(item){
+                        if(item.id == id){
+                          item = newItem; //if this does not work try notification[key] = new Notification instead
+                          return true;
+                        }
+                      });
+                    },
+                    error: function(jqXHR, textStatus, errorThrown){
+                        console.log("Unable to reach backend");
+                    }
+                });
+              },
+
+              delete: function(id){
+                var url = tokenReplace(urls.itemsDelete,[id]);
+                $.ajax({
+                    type: 'DELETE',
+                    url: url,
+                    success: function(data){
+                      _.detect(items,function(key, item){
+                        if(item.id == id){
+                          removeValue(items, key);
+                          return true;
+                        }
+                      });
+                    },
+                    error: function(jqXHR, textStatus, errorThrown){
+                        console.log("Unable to reach backend");
+                    }
+                });
+                
+              }
+            
+            },    
+
+            notifications : {
+
+              get: function(){
+                return notifications;
+              },
+
+              fetch: function(limit){
+                limit = limit || limit;
+                var url = tokenReplace(urls.notifications,[limit]);
+
+                $.get(url, item, function(data){
+                    notifications = JSON.parse(data);
+                },this).error(function(){console.log("Unable to reach backend")});
+              },
+
+              add: function(notification){
+                $.post(urls.notificationAdd,notification,function(data){
+                    notifications.push(notification);
+                }).error(function(){console.log("Unable to reach backend")});
+
+
+              },
+
+              update: function(id,newNotification){
+                var url = tokenReplace(urls.notificationsUpdate,[id]);
+                $.ajax({
+                    type: 'PUT',
+                    contentType: 'application/json',
+                    url: url,
+                    dataType: "json",
+                    data: newNotification,
+                    success: function(data){
+                      _.detect(notifications,function(notification){
+                        if(notification.id == id){
+                          notification = newNotification; //if this does not work try notification[key] = new Notification instead
+                          return true;
+                        }
+                      });
+                    },
+                    error: function(jqXHR, textStatus, errorThrown){
+                        console.log("Unable to reach backend");
+                    }
+                });
+              },
+
+              delete: function(id){
+                var url = tokenReplace(urls.notificationsDelete,[id]);
+                $.ajax({
+                    type: 'DELETE',
+                    url: url,
+                    success: function(data){
+                      _.detect(notifications,function(key, notification){
+                        if(notification.id == id){
+                          removeValue(notifications, key);
+                          return true;
+                        }
+                      });
+                    },
+                    error: function(jqXHR, textStatus, errorThrown){
+                        console.log("Unable to reach backend");
+                    }
+                });
+
+              }
+
+            }
+
+          };
+
+
+          return methods;
+
+
+        })($);
