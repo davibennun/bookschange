ffde2a64b69987bbda7a58dbb07527ab9602998c
Davi Nunes <futurodavi@hotmail.com>
2013-01-30 09:51:21 -0200

    sdf

--- backend/AuthMiddleware.class.php
+++ backend/AuthMiddleware.class.php
@@ -8,8 +8,11 @@ class AuthMiddleware extends \Slim\Middleware
         // Get reference to application
         $app = &$this->app;
         
+        $res = $app->response();
+        $data = json_encode($res->body());
+        print_r($data);
         //process and get facebook id
-        $app->fb_id = "123456";
+        $app->fb_id = $data["fb_id"] || "123456";
 
         // Run inner middleware and application
         $this->next->call();        
