3b6856c9614ebe99c7f7cebe449ddb742c7ee069
Davi Nunes <futurodavi@hotmail.com>
2013-01-31 16:25:59 -0200

    changes

--- prototype/app.php
+++ prototype/app.php
@@ -258,7 +258,7 @@ $app_name = idx($app_info, 'name', '');
             
 
 
-            mozilla = window.location.hostname + '/manifest.webapp';
+            mozilla = "http://"+window.location.hostname + '/manifest.webapp';
             console.log(mozilla);
             mozillaInstall = function () {
                 var installRequest = navigator.mozApps.install(mozilla);
