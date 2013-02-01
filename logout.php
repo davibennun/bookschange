43cb40d71563314b4ebfbe613f69ec6d80849a85
Davi Nunes <futurodavi@hotmail.com>
2013-01-31 23:56:37 -0200

    changes

--- logout.php
+++ logout.php
@@ -1,5 +1,15 @@
 <?php
 
+require_once('AppInfo.php');
+require_once('sdk/src/facebook.php');
+
+$facebook = new Facebook(array(
+  'appId'  => AppInfo::appID(),
+  'secret' => AppInfo::appSecret(),
+  'sharedSession' => true,
+  'trustForwarded' => true,
+));
+
 setcookie('fbs_'.$facebook->getAppId(), '', time()-100, '/', 'domain.com');
 session_destroy();
 header('Location: /');
