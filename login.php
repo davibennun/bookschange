e4ca04bbf5dd9da9b0c3cfbd212ffba0ca29ae17
Davi Nunes <futurodavi@hotmail.com>
2013-02-01 14:59:49 -0200

    changes

---
+++ login.php
@@ -0,0 +1,17 @@
+<?php
+
+require_once('AppInfo.php');
+
+try{
+  $facebook = new Facebook(array(
+    'appId'  => AppInfo::appID(),
+    'secret' => AppInfo::appSecret(),
+    'sharedSession' => true,
+    'trustForwarded' => true,
+    'cookie'=>true
+  ));
+}catch(Exception $e){
+  var_dump($e);
+}
+
+$user_id = $facebook->getUser();
